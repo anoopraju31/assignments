dc91e184b604180c243b09ecea018d4e978e8c67
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 23:18:29 +0530

    Easy Problem - Vowel Count

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,25 @@
 */
 
 function countVowels(str) {
-    // Your code here
+	let ls = str.toLowerCase()
+
+	let dic = {
+		a: 0,
+		e: 0,
+		i: 0,
+		o: 0,
+		u: 0,
+	}
+
+	for (let l of ls) {
+		if (dic.hasOwnProperty(l)) {
+			dic[l] += 1
+		} else {
+			dic[l] = 1
+		}
+	}
+
+	return dic.a + dic.e + dic.i + dic.o + dic.u
 }
 
-module.exports = countVowels;
+module.exports = countVowels
